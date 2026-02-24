81c6d2ddf6137c29003679657df916d6ccc1ad1d
tibo05102007-byte <tibo05102007@gmail.com>
2026-02-25 00:56:15 +0500

    Update import_products.js

--- app/scripts/import_products.js
+++ app/scripts/import_products.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const SRC_DIR = 'c:\\Users\\User\\Documents\\Adobe\\bodiu\\products_image';
+const SRC_DIR = 'c:/Users/User/Documents/Adobe/bodiu/products_image';
 const DEST_DIR = path.join(__dirname, '../public/images/products/auto');
 
 if (!fs.existsSync(DEST_DIR)) {
